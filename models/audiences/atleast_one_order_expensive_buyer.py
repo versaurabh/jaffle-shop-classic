09ee698af991f5fce2711a6874544ee0a3c7a2a3
Saurabh Verma <saurabh.verma@oportun.com>
2025-03-07 23:28:16 +0530

    http based dbt py model

--- models/audiences/atleast_one_order_expensive_buyer.py
+++ models/audiences/atleast_one_order_expensive_buyer.py
@@ -1,19 +1,20 @@
-# def model(dbt, session):
-#     dbt.config(
-#         submission_method="serverless_cluster",
-#         create_notebook=False,
-#     )
+def model(dbt, session):
+    dbt.config(
+        # submission_method="serverless_cluster",
+        http_path = "/sql/1.0/warehouses/6177e39cfc5194cf",
+        create_notebook=False,
+    )
 
-#     cols_needed = {
-#      'atleast_one_order_completed': ['atleast_one_order_completed'],
-#      'expensive_buyer': ['avg_amt', 'expensive_buyer'],  
-#     }
+    cols_needed = {
+     'atleast_one_order_completed': ['atleast_one_order_completed'],
+     'expensive_buyer': ['avg_amt', 'expensive_buyer'],  
+    }
 
-#     # these are DAG-aware, and return dataframes
-#     atleast_one_order_completed = dbt.ref("atleast_one_order_completed")
-#     expensive_buyer = dbt.ref("expensive_buyer")
+    # these are DAG-aware, and return dataframes
+    atleast_one_order_completed = dbt.ref("atleast_one_order_completed")
+    expensive_buyer = dbt.ref("expensive_buyer")
 
-#     # your final 'select' statement
-#     # df = atleast_one_order_completed.join(expensive_buyer, )
+    # your final 'select' statement
+    df = atleast_one_order_completed.join(expensive_buyer, "customer_id")
 
-#     return atleast_one_order_completed
+    return df
