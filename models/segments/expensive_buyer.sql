5ced9304fd550576c321ccfeacc5c6bc821bcc02
Saurabh Verma <saurabh.verma@oportun.com>
2025-03-07 21:02:26 +0530

    expensive_buyer segment

--- models/segments/expensive_buyer.sql
+++ models/segments/expensive_buyer.sql
@@ -10,14 +10,34 @@ orders as (
 
 ),
 
+payments as (
+
+    select * from {{ ref('stg_payments') }}
+
+),
+
 final as (
-    select customers.customer_id, customers.first_name, customers.last_name, avg(amount) as avg_amt, true as expensive_buyer
-    from customers, orders
-    where customers.customer_id = orders.customer_id
-    and orders.status = 'completed'
-  group by customers.customer_id, customers.first_name, customers.last_name
+select
+    stg_customers.customer_id,
+    stg_customers.first_name,
+    stg_customers.last_name,
+    avg(amount) as avg_amt,
+    true as expensive_buyer
+  from
+    stg_customers,
+    stg_orders,
+    stg_payments
+  where
+    stg_customers.customer_id = stg_orders.customer_id
+    and stg_orders.status = 'completed'
+    and stg_orders.order_id = stg_payments.order_id
+  group by
+    stg_customers.customer_id,
+    stg_customers.first_name,
+    stg_customers.last_name
   having avg_amt > 20
-  order by avg_amt desc
+  order by
+    avg_amt desc
 )
 
 select * from final
