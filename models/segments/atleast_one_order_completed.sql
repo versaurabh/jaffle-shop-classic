67d5a0dcad95ade63de90448f0f425f12c7d7a74
Saurabh Verma <saurabh.verma@oportun.com>
2025-03-07 20:34:50 +0530

    atleast_one_order_completed segment

--- models/segments/atleast_one_order_completed.sql
+++ models/segments/atleast_one_order_completed.sql
@@ -11,7 +11,7 @@ orders as (
 ),
 
 final as (
-    select distinct customers.customer_id, customers.first_name, customers.last_name, 'TRUE' as atleast_one_order_completed
+    select distinct customers.customer_id, customers.first_name, customers.last_name, TRUE as atleast_one_order_completed
     from customers, orders
     where customers.customer_id = orders.customer_id
     and orders.status = 'returned'
