e49b1f643eebba2a154ad07c4eedbd4352f2a08d
Saurabh Verma <saurabh.verma@oportun.com>
2025-03-07 23:09:17 +0530

    dbt_artifact

--- models/audiences/atleast_one_order_expensive_buyer.py
+++ models/audiences/atleast_one_order_expensive_buyer.py
@@ -1,19 +1,19 @@
-def model(dbt, session):
-    dbt.config(
-        submission_method="serverless_cluster",
-        create_notebook=False,
-    )
+# def model(dbt, session):
+#     dbt.config(
+#         submission_method="serverless_cluster",
+#         create_notebook=False,
+#     )
 
-    cols_needed = {
-     'atleast_one_order_completed': ['atleast_one_order_completed'],
-     'expensive_buyer': ['avg_amt', 'expensive_buyer'],  
-    }
+#     cols_needed = {
+#      'atleast_one_order_completed': ['atleast_one_order_completed'],
+#      'expensive_buyer': ['avg_amt', 'expensive_buyer'],  
+#     }
 
-    # these are DAG-aware, and return dataframes
-    atleast_one_order_completed = dbt.ref("atleast_one_order_completed")
-    expensive_buyer = dbt.ref("expensive_buyer")
+#     # these are DAG-aware, and return dataframes
+#     atleast_one_order_completed = dbt.ref("atleast_one_order_completed")
+#     expensive_buyer = dbt.ref("expensive_buyer")
 
-    # your final 'select' statement
-    # df = atleast_one_order_completed.join(expensive_buyer, )
+#     # your final 'select' statement
+#     # df = atleast_one_order_completed.join(expensive_buyer, )
 
-    return atleast_one_order_completed
+#     return atleast_one_order_completed
